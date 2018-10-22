ddd4e679fc84a454ce15fd2f6be2fbdc49b6a5e0
rrivera1980 <44072914+rrivera1980@users.noreply.github.com>
2018-10-22 10:10:42 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var names = new List<string> {"Rolando", "Diego", "Dana" };
+			foreach (var name in names)
+			{
+				Console.WriteLine($"Hello {name.ToUpper ()}!");
+			}
+        }
+    }
+}
